9f66968ff132070d2b7961e5d53d5f1c4c3d24ac
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-19 16:28:46 +0100

    feat: add random number in page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 //Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 //Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+const randArray = generateRandomArray(5, 1, 100);
+console.log(randArray);
+//scrivo l'array con i numeri inseriti randomicamente nel DOM
+document.querySelector('#text').innerHTML = randArray
+
+function generateRandomArray(arrayLength, numMin, numMax) {
+    // Creo un array vuoto
+    const randomNumbersArray = [];
+
+    // finche non ci sono arrayLength numeri nell'array:
+    while(randomNumbersArray.length < arrayLength) {
+        // genero un numero random e controllo che non sia già stato inserito
+        const randNumber = getRndInteger(numMin, numMax);
+        if(!randomNumbersArray.includes(randNumber)) {
+            randomNumbersArray.push(randNumber);
+        }
+    }
+    
+    return randomNumbersArray;
+}
+
+
+//funzione che crea numeri random
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
